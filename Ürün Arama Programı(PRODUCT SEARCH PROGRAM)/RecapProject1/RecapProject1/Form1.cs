40d0852cf4abc66363cb3397523af0654f0220a7
alpiren19 <alperengrhan@gmail.com>
2022-09-05 23:55:26 +0300

    c#

---
+++ Ürün Arama Programı(PRODUCT SEARCH PROGRAM)/RecapProject1/RecapProject1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecapProject1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SelectCategories();
+            ListCategories();
+        }
+
+        private void ListCategories()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                dgwProduct.DataSource = context.Products.ToList(); 
+            }
+        }
+
+        private void ListProductByCategory(int categoryId)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                dgwProduct.DataSource = context.Products.Where(p=>p.CategoryId==categoryId).ToList();
+            }
+        }
+        private void ListProductByProductName(string key)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                dgwProduct.DataSource = context.Products.Where(p => p.ProductName.ToLower().Contains(key.ToLower())).ToList();
+            }
+        }
+        private void SelectCategories()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+               cbxCategory.DataSource= context.Categories.ToList();
+                cbxCategory.DisplayMember = "CategoryName";
+                cbxCategory.ValueMember = "CategoryId";
+            }
+        }
+
+        private void cbxCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            try
+            {
+                ListProductByCategory(Convert.ToInt32(cbxCategory.SelectedValue ));
+            }
+            catch (Exception)
+            {
+
+                
+            }
+            
+        }
+
+        private void tbxSearch_TextChanged(object sender, EventArgs e)
+        {
+            string key = tbxSearch.Text;
+            if (string.IsNullOrEmpty(key))
+            {
+                ListCategories();
+            }
+            else
+            {
+                ListProductByProductName(tbxSearch.Text);
+            }
+          
+        }
+    }
+}
